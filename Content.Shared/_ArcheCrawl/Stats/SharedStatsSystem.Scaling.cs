23aaca9e6bc905c10bb7b526d38be5dd14d86bde
EmoGarbage404 <retron404@gmail.com>
2023-05-26 18:09:03 -0400

    Make health scale with stats

---
+++ Content.Shared/_ArcheCrawl/Stats/SharedStatsSystem.Scaling.cs
@@ -0,0 +1,23 @@
+﻿using Content.Shared._ArcheCrawl.Stats.Components;
+using Content.Shared.Mobs.Systems;
+
+namespace Content.Shared._ArcheCrawl.Stats;
+
+public abstract partial class SharedStatsSystem
+{
+    [Dependency] private readonly MobThresholdSystem _mobThreshold = default!;
+
+    public void InitializeScaling()
+    {
+        SubscribeLocalEvent<StatVariedThresholdComponent, StatChangedEvent>(OnStatVariedDeathThresholdChanged);
+    }
+
+    private void OnStatVariedDeathThresholdChanged(EntityUid uid, StatVariedThresholdComponent component, ref StatChangedEvent args)
+    {
+        if (args.Stat.ID != component.ScalingStat)
+            return;
+
+        var val = MathF.Round(component.BaseThreshold + args.NewValue * component.ThresholdPerStat);
+        _mobThreshold.SetMobStateThreshold(uid, val, component.TargetState);
+    }
+}
